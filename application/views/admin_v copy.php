ee061f544a2be6129c31ca0c714447683619f22a
unknown <nurhayatifitriani77@gmail.com>
2022-02-24 07:18:00 +0700

    hello world

---
+++ application/views/admin_v copy.php
@@ -0,0 +1,383 @@
+<!DOCTYPE html>
+<!--
+Template Name: Metronic - Bootstrap 4 HTML, React, Angular 11 & VueJS Admin Dashboard Theme
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: https://1.envato.market/EA4JP
+Renew Support: https://1.envato.market/EA4JP
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<html lang="en">
+	<!--begin::Head-->
+	
+<!-- Mirrored from preview.keenthemes.com/metronic/demo2/crud/forms/validation/form-controls.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 13 Jan 2022 06:38:04 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src= '../../../../../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-5FS8GGP');</script>
+		<!-- End Google Tag Manager -->
+		<meta charset="utf-8" />
+		<title>Pendaftaran SWAB || SYAMRABU</title>
+		<meta name="description" content="Form controls validation" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<!-- <link rel="canonical" href="https://keenthemes.com/metronic" /> -->
+		<!--begin::Fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+		<!--end::Fonts-->
+		<!--begin::Global Theme Styles(used by all pages)-->
+		<link href="assets/plugins/global/plugins.bundle5883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+		<link href="assets/plugins/custom/prismjs/prismjs.bundle5883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+		<link href="assets/css/style.bundle5883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+		<!--end::Global Theme Styles-->
+		<!--begin::Layout Themes(used by all pages)-->
+		<!--end::Layout Themes-->
+		<link rel="shortcut icon" href="assets/logot.png" />
+		<!-- Hotjar Tracking Code for keenthemes.com -->
+		<script>(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:1070954,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');</script>
+	</head>
+	<!--end::Head-->
+	<!--begin::Body-->
+	<body id="kt_body" style="background-image: url(assets/media/bg/bg-10.jpg)" class="quick-panel-right demo-panel-right offcanvas-right header-fixed subheader-enabled page-loading">
+		<!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+		</noscript>
+		<!-- End Google Tag Manager (noscript) -->
+		<!--begin::Main-->
+		<!--begin::Header Mobile-->
+		<div id="kt_header_mobile" class="header-mobile">
+			<!--begin::Logo-->
+			<a href="">
+				<img alt="Logo" src="assets/logo.png" class="logo-default max-h-30px" />
+			</a>
+			<!--end::Logo-->
+			<!--begin::Toolbar-->
+			<!--end::Toolbar-->
+		</div>
+		<!--end::Header Mobile-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Page-->
+			<div class="d-flex flex-row flex-column-fluid page">
+				<!--begin::Wrapper-->
+				<div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+					<!--begin::Header-->
+				
+					<!--end::Header-->
+					<!--begin::Content-->
+					<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+						<!--begin::Subheader-->
+						<div class="subheader py-2 py-lg-12 subheader-transparent" id="kt_subheader">
+							<div class="container d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+								<!--begin::Info-->
+								<div class="d-flex align-items-center flex-wrap mr-1">
+									<!--begin::Heading-->
+									<div class="d-flex flex-column">
+										<!--begin::Title-->
+										<!-- <h2 class="text-white font-weight-bold my-2 mr-5">RSUD SYAMRABU BANGKALAN</h2> -->
+										<!--end::Title-->
+										<!--begin::Breadcrumb-->
+										<!--end::Breadcrumb-->
+									</div>
+									<!--end::Heading-->
+								</div>
+								<!--end::Info-->
+								<!--begin::Toolbar-->
+								<!--end::Toolbar-->
+							</div>
+						</div>
+						<!--end::Subheader-->
+						<!--begin::Entry-->
+						<div class="d-flex flex-column-fluid">
+							<!--begin::Container-->
+							<div class="container">
+								<!--begin::Card-->
+								<div class="row">
+									<div class="col-lg-12">
+										<!--begin::Card-->
+										<div class="card card-custom example example-compact">
+											<div class="card-header">
+												<h3 class="card-title"><img src="assets/logot.png" >Formulir Penyelidikan Epidemiologi Coronavirus Disease (COVID 19)</h3>
+												<div class="card-toolbar">
+													<div class="example-tools justify-content-center">
+														<!-- <span class="example-toggle" data-toggle="tooltip" title="View code"></span> -->
+													</div>
+												</div>
+											</div>
+											<!--begin::Form-->
+											<form class="form">
+												<div class="card-body">
+													<div class="form-group row">
+														<div class="col-lg-4">
+															<label>Identitas Pasien:</label>
+															<span class="form-text text-muted">Mohon Masukkan Nama Lengkap Anda</span>
+															<input type="text" class="form-control" id="nama" placeholder="Nama Lengkap" required />
+														</div>
+														<div class="col-lg-4">
+															<!-- <label>Email:</label> -->
+															<span class="form-text text-muted">Mohon Masukkan NIK Sesuai KTP</span>
+															<input type="text" class="form-control" id="nik" placeholder="NIK" />
+														</div>
+														<div class="col-lg-4">
+															<!-- <label>Username:</label> -->
+															<span class="form-text text-muted">Masukkan Nama Orang tua sesuai KK</span>
+															<div class="input-group">
+																<div class="input-group-prepend">
+																	<span class="input-group-text">
+																		<i class="la la-user"></i>
+																	</span>
+																</div>
+																<input type="text" class="form-control" id="nama_ortu" placeholder="Nama Orang Tua" />
+															</div>
+														</div>
+														<div class="col-lg-4">
+															<!-- <label>Email:</label> -->
+															<span class="form-text text-muted">Mohon Masukkan Tgl Lahir Sesuai KTP</span>
+															<input type="date" class="form-control" id="ttl" placeholder="Pilih Tanggal" />
+														</div>
+														<div class="col-lg-4">
+															<!-- <label>Username:</label> -->
+															<span class="form-text text-muted">Masukkan Umur Anda</span>
+															<div class="input-group">
+																<div class="input-group-prepend">
+																	<span class="input-group-text">
+																		<i class="la la-info-circle"></i>
+																	</span>
+																</div>
+																<input type="text" class="form-control" id="umur" placeholder="Umur" />
+															</div>
+														</div>
+														<div class="col-lg-4">
+															<!-- <label>Jenis:</label> -->
+															<span class="form-text text-muted">Pilih Jenis Kelamin</span>
+															<div class="radio-inline" id="jk">
+																<label class="radio radio-solid">
+																<input type="radio" name="example_2" checked="checked" value="2" />
+																<span></span>Laki-Laki</label>
+																<label class="radio radio-solid">
+																<input type="radio" name="example_2" value="2" />
+																<span></span>Perempuan</label>
+															</div>
+														</div>
+														<div class="col-lg-4">
+															<!-- <label>Username:</label> -->
+															<span class="form-text text-muted">Masukkan Pekerjaan Anda Sesuai KTP</span>
+															<div class="input-group">
+																<div class="input-group-prepend">
+																	<span class="input-group-text">
+																		<i class="la la-info-circle"></i>
+																	</span>
+																</div>
+																<input type="text" class="form-control" id="pekerjaan" placeholder="Pekerjaan" />
+															</div>
+														</div>
+													</div>
+													<!-- <div class="form-group row">
+														<div class="col-lg-4">
+															<label>Contact:</label>
+															<input type="email" class="form-control" placeholder="Enter contact number" />
+															<span class="form-text text-muted">Please enter your contact</span>
+														</div>
+														<div class="col-lg-4">
+															<label>Fax:</label>
+															<div class="input-group">
+																<div class="input-group-prepend">
+																	<span class="input-group-text">
+																		<i class="la la-info-circle"></i>
+																	</span>
+																</div>
+																<input type="text" class="form-control" placeholder="Fax number" />
+															</div>
+															<span class="form-text text-muted">Please enter fax</span>
+														</div>
+														<div class="col-lg-4">
+															<label>Address:</label>
+															<div class="input-group">
+																<input type="text" class="form-control" placeholder="Enter your address" />
+																<div class="input-group-append">
+																	<span class="input-group-text">
+																		<i class="la la-map-marker"></i>
+																	</span>
+																</div>
+															</div>
+															<span class="form-text text-muted">Please enter your address</span>
+														</div>
+													</div>
+													<div class="form-group row">
+														<div class="col-lg-4">
+															<label>Postcode:</label>
+															<div class="input-group">
+																<div class="input-group-prepend">
+																	<span class="input-group-text">
+																		<i class="la la-bookmark-o"></i>
+																	</span>
+																</div>
+																<input type="text" class="form-control" placeholder="Enter your postcode" />
+															</div>
+															<span class="form-text text-muted">Please enter your postcode</span>
+														</div>
+														<div class="col-lg-4">
+															<label>User Group:</label>
+															<div class="radio-inline">
+																<label class="radio radio-solid">
+																<input type="radio" name="example_2" checked="checked" value="2" />
+																<span></span>Sales Person</label>
+																<label class="radio radio-solid">
+																<input type="radio" name="example_2" value="2" />
+																<span></span>Customer</label>
+															</div>
+															<span class="form-text text-muted">Please select user group</span>
+														</div>
+													</div> -->
+												</div>
+												<div class="card-footer">
+													<div class="row">
+														<div class="col-lg-4"></div>
+														<div class="col-lg-8">
+															<button type="reset" onClick="tambah()" id="tombolTambah" class="btn btn-primary mr-2">Submit</button>
+															<button type="reset" class="btn btn-secondary">Cancel</button>
+														</div>
+													</div>
+												</div>
+											</form>
+											<!--end::Form-->
+										</div>
+										<!--end::Card-->
+									</div>
+										</div>
+										<!--end::Card-->
+									</div>
+								</div>
+							</div>
+							<!--end::Container-->
+						</div>
+						<!--end::Entry-->
+					</div>
+					<!--end::Content-->
+					<!--begin::Footer-->
+					<div class="footer bg-white py-4 d-flex flex-lg-column" id="kt_footer">
+						<!--begin::Container-->
+						<div class="container d-flex flex-column flex-md-row align-items-center justify-content-between">
+							<!--begin::Copyright-->
+							<div class="text-dark order-2 order-md-1">
+								<span class="text-muted font-weight-bold mr-2">2022©</span>
+								<a href="http://keenthemes.com/metronic" target="_blank" class="text-dark-75 text-hover-primary">Keenthemes</a>
+							</div>
+							<!--end::Copyright-->
+							<!--begin::Nav-->
+							<div class="nav nav-dark order-1 order-md-2">
+								<a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pr-3 pl-0">About</a>
+								<a href="http://keenthemes.com/metronic" target="_blank" class="nav-link px-3">Team</a>
+								<a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pl-3 pr-0">Contact</a>
+							</div>
+							<!--end::Nav-->
+						</div>
+						<!--end::Container-->
+					</div>
+					<!--end::Footer-->
+				</div>
+				<!--end::Wrapper-->
+			</div>
+			<!--end::Page-->
+		</div>
+		<!--end::Main-->
+		<!-- begin::User Panel-->
+		<!-- end::User Panel-->
+		<!--begin::Quick Panel-->
+		<!--end::Quick Panel-->
+		<!--begin::Chat Panel-->
+		<!--end::Chat Panel-->
+		<!--begin::Scrolltop-->
+		<div id="kt_scrolltop" class="scrolltop">
+			<span class="svg-icon">
+				<!--begin::Svg Icon | path:/metronic/theme/html/demo2/dist/assets/media/svg/icons/Navigation/Up-2.svg-->
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+					<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+						<polygon points="0 0 24 0 24 24 0 24" />
+						<rect fill="#000000" opacity="0.3" x="11" y="10" width="2" height="10" rx="1" />
+						<path d="M6.70710678,12.7071068 C6.31658249,13.0976311 5.68341751,13.0976311 5.29289322,12.7071068 C4.90236893,12.3165825 4.90236893,11.6834175 5.29289322,11.2928932 L11.2928932,5.29289322 C11.6714722,4.91431428 12.2810586,4.90106866 12.6757246,5.26284586 L18.6757246,10.7628459 C19.0828436,11.1360383 19.1103465,11.7686056 18.7371541,12.1757246 C18.3639617,12.5828436 17.7313944,12.6103465 17.3242754,12.2371541 L12.0300757,7.38413782 L6.70710678,12.7071068 Z" fill="#000000" fill-rule="nonzero" />
+					</g>
+				</svg>
+				<!--end::Svg Icon-->
+			</span>
+		</div>
+		<!--end::Scrolltop-->
+		<!--begin::Sticky Toolbar-->
+		<!--end::Sticky Toolbar-->
+		<!--begin::Demo Panel-->
+		<!--end::Demo Panel-->
+    <!-- script -->
+    <script>
+        function tambah() {
+          $("#tombolTambah").html('<i class="fas fa-spinner fa-pulse"></i> Memproses..')
+          var nama = $("#nama").val()
+          var nik = $("#nik").val()
+          var nama_ortu = $("#nama_ortu").val()
+          var ttl = $("#ttl").val()
+          var umur = $("#umur").val()
+          var jk = $("#jk").val()
+          var pekerjaan = $("#pekerjaan").val()
+    // if (rule == null) {
+    //   rule = 0;
+    // }
+          $.ajax({
+            url: 'swab/tambah_data',
+            method: 'post',
+            data: {
+              nama: nama,
+              nik: nik,
+              nama_ortu: nama_ortu,
+              ttl: ttl,
+              umur: umur,
+              jk: jk,
+              pekerjaan: pekerjaan
+            },
+            dataType: 'json',
+            success: function(data) {
+              // console.log(data);
+              // if (data == "") {
+                
+                $("#nama").val("")
+                $("#nik").val("")
+                $("#nama_ortu").val("")
+                $("#ttl").val("")
+                $("#umur").val("")
+                $("#jk").val("")
+                $("#pekerjaan").val("")
+              // } else {
+                // data = data.replace("<p>", "");
+                // data = data.replace("</p>", "");
+                $('#pesanErrorTambah').html(data)
+              // }
+
+              $("#tombolTambah").html('Data Sudah Tersimpan')
+              // $("#tombolTambah").html('Tambah')
+              // $("#modalTambah").modal('hide');
+                // tampilkan();
+              // $("#tombolTambah").html('Tambah')
+              // tampilkan();
+            }
+          });
+        }
+    </script>
+
+		<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
+		<!--begin::Global Config(global config for global JS scripts)-->
+		<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1200 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#6993FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#F3F6F9", "dark": "#212121" }, "light": { "white": "#ffffff", "primary": "#E1E9FF", "secondary": "#ECF0F3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#212121", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#ECF0F3", "gray-300": "#E5EAEE", "gray-400": "#D6D6E0", "gray-500": "#B5B5C3", "gray-600": "#80808F", "gray-700": "#464E5F", "gray-800": "#1B283F", "gray-900": "#212121" } }, "font-family": "Poppins" };</script>
+		<!--end::Global Config-->
+		<!--begin::Global Theme Bundle(used by all pages)-->
+		<script src="assets/plugins/global/plugins.bundle5883.js?v=7.2.9"></script>
+		<script src="assets/plugins/custom/prismjs/prismjs.bundle5883.js?v=7.2.9"></script>
+		<script src="assets/js/scripts.bundle5883.js?v=7.2.9"></script>
+		<!--end::Global Theme Bundle-->
+		<!--begin::Page Scripts(used by this page)-->
+		<script src="assets/js/pages/crud/forms/validation/form-controls5883.js?v=7.2.9"></script>
+		<!--end::Page Scripts-->
+	</body>
+	<!--end::Body-->
+
+<!-- Mirrored from preview.keenthemes.com/metronic/demo2/crud/forms/validation/form-controls.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 13 Jan 2022 06:38:05 GMT -->
+</html>
---
+++ application/views/admin_v.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<!--
+Template Name: Metronic - Bootstrap 4 HTML, React, Angular 11 & VueJS Admin Dashboard Theme
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: https://1.envato.market/EA4JP
+Renew Support: https://1.envato.market/EA4JP
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<html lang="en">
+	<!--begin::Head-->
+	
+<!-- Mirrored from preview.keenthemes.com/metronic/demo2/custom/pages/login/classic/login-4.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 13 Jan 2022 06:40:32 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src= '../../../../../../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-5FS8GGP');</script>
+		<!-- End Google Tag Manager -->
+		<meta charset="utf-8" />
+		<title>Login Admin</title>
+		<meta name="description" content="Login page example" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<link rel="canonical" href="https://keenthemes.com/metronic" />
+		<!--begin::Fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+		<!--end::Fonts-->
+		<!--begin::Page Custom Styles(used by this page)-->
+		<link href="assets/css/pages/login/classic/login-45883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+		<!--end::Page Custom Styles-->
+		<!--begin::Global Theme Styles(used by all pages)-->
+		<link href="assets/plugins/global/plugins.bundle5883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+		<link href="assets/plugins/custom/prismjs/prismjs.bundle5883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+		<link href="assets/css/style.bundle5883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+		<!--end::Global Theme Styles-->
+		<!--begin::Layout Themes(used by all pages)-->
+		<!--end::Layout Themes-->
+		<link rel="shortcut icon" href="assets/logot.png" />
+		<!-- Hotjar Tracking Code for keenthemes.com -->
+		<script>(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:1070954,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');</script>
+	</head>
+	<!--end::Head-->
+	<!--begin::Body-->
+	<body id="kt_body" style="background-image: url(assets/media/bg/bg-9.jpg)" class="quick-panel-right demo-panel-right offcanvas-right header-fixed subheader-enabled page-loading">
+		<!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+		</noscript>
+		<!-- End Google Tag Manager (noscript) -->
+		<!--begin::Main-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Login-->
+			<div class="login login-4 login-signin-on d-flex flex-row-fluid" id="kt_login">
+				<div class="d-flex flex-center flex-row-fluid bgi-size-cover bgi-position-top bgi-no-repeat" style="background-image: url('assets/media/bg/bg-9.jpg');">
+					<div class="login-form text-center p-7 position-relative overflow-hidden">
+						<!--begin::Login Header-->
+						
+						<div class="d-flex flex-center mb-15">
+							<a href="#">
+								<img src="assets/logo.png" class="max-h-75px" alt="" />
+							</a>
+						</div>
+						<!--end::Login Header-->
+						<!--begin::Login Sign in form-->
+						<div class="login-signin">
+							<form class="form" action='#' method="POST">
+								<div class="form-group mb-5 <?php if (isset($error)) {
+																	if ($error[0] == "email") echo "alert-validate";
+																} ?>" data-validate="<?php if (isset($error)) {
+																							if ($error[0] == "email") {
+																								echo $error[1];
+																							} else {
+																								echo "Silahkan isi Email.";
+																							}
+																						} else {
+																							echo "Silahkan isi Email.";
+																						} ?>" id="pesanEmail">
+									<input class="form-control h-auto form-control-solid py-4 px-8" type="text" placeholder="Email" name="email" id="email" autocomplete="off" />
+								</div>
+								<div class="form-group mb-5 <?php if (isset($error)) {
+																	if ($error[0] == "pass") echo "alert-validate";
+																} ?>" data-validate="<?php if (isset($error)) {
+																							if ($error[0] == "pass") {
+																								echo $error[1];
+																							} else {
+																								echo "Silahkan isi password.";
+																							}
+																						} else {
+																							echo "Silahkan isi password.";
+																						} ?>" id="pesanPass">
+									<input class="form-control h-auto form-control-solid py-4 px-8" type="password" placeholder="Password" id="pass" name="pass"/>
+								</div>
+								<button id="tombolLogin" class="btn btn-success font-weight-bold px-9 py-4 my-3 mx-4">Login</button>
+							</form>
+						</div>
+						<!--end::Login Sign in form-->
+					</div>
+				</div>
+			</div>
+			<!--end::Login-->
+		</div>
+		<!--end::Main-->
+		<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
+		<!--begin::Global Config(global config for global JS scripts)-->
+		<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1200 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#6993FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#F3F6F9", "dark": "#212121" }, "light": { "white": "#ffffff", "primary": "#E1E9FF", "secondary": "#ECF0F3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#212121", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#ECF0F3", "gray-300": "#E5EAEE", "gray-400": "#D6D6E0", "gray-500": "#B5B5C3", "gray-600": "#80808F", "gray-700": "#464E5F", "gray-800": "#1B283F", "gray-900": "#212121" } }, "font-family": "Poppins" };</script>
+		<!--end::Global Config-->
+		<!--begin::Global Theme Bundle(used by all pages)-->
+		<script src="assets/plugins/global/plugins.bundle5883.js?v=7.2.9"></script>
+		<script src="assets/plugins/custom/prismjs/prismjs.bundle5883.js?v=7.2.9"></script>
+		<script src="assets/js/scripts.bundle5883.js?v=7.2.9"></script>
+		<!--end::Global Theme Bundle-->
+		<!--begin::Page Scripts(used by this page)-->
+		<script src="assets/js/pages/custom/login/login-general5883.js?v=7.2.9"></script>
+		<!--end::Page Scripts-->
+	</body>
+	<!--end::Body-->
+
+<!-- Mirrored from preview.keenthemes.com/metronic/demo2/custom/pages/login/classic/login-4.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 13 Jan 2022 06:40:33 GMT -->
+</html>
